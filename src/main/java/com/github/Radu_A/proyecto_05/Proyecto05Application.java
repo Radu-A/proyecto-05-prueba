2bb7be4cebc07343c7ce908ba84c4bec4523a23f
Radu-A <elcastravete@gmail.com>
2026-05-06 12:46:07 +0200

    first commit

---
+++ src/main/java/com/github/Radu_A/proyecto_05/Proyecto05Application.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.proyecto_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Radu_A/proyecto_05/Proyecto05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.proyecto_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
